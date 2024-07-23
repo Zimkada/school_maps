c7dadcf0f10d9b2288c56088d91bcea569a38c9c
zimkada <zimkada@gmail.com>
2024-07-23 22:37:35 +0100

    Ajout de graphiques des effectifs de chaque collège à la carte

--- banikoara_map.py
+++ banikoara_map.py
@@ -7,9 +7,12 @@ Created on Sat Jul 20 00:21:45 2024
 
 #Importation des bibliothèques
 import pandas as pd
+import matplotlib.pyplot as plt
 import folium
 from geopy.distance import geodesic
 import geopandas as gpd
+import base64
+from io import BytesIO
 
 #Chargement des coordonnées géo
 
@@ -26,6 +29,26 @@ bounds = [[11.2500, 2.3800], [11.3500, 2.4800]]
 # Définir les limites de la carte
 map_bnk.fit_bounds(bounds)
 
+def create_graph(row):
+    labels = ['EFF_M', 'EFF_F']
+    sizes = [row['EFF_M'], row['EFF_F']]
+    colors = ['#ff9999','#66b3ff']
+    
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.title(f"Effectifs de {row['NOM']}")
+    
+    buf = BytesIO()
+    plt.savefig(buf, format='png', bbox_inches='tight')
+    plt.close(fig)
+    buf.seek(0)
+    img_base64 = base64.b64encode(buf.read()).decode('utf-8')
+    
+    return f'data:image/png;base64,{img_base64}'
+
+
+
 # Placement des collèges sur la carte
 for index, row in df.iterrows():
     # Marqueur avec icône en forme de goutte d'eau
@@ -41,6 +64,16 @@ for index, row in df.iterrows():
         icon=folium.DivIcon(
             html=f'<div style = "font-family: Arial, sans-serif; font-size:12px; front-weight = bold; front-style = italic; color: red; background-color: gray; opacity = 0.8; text-decoration = underline; text-align: left; border: 1px solid black;"> {row["EFF"]}<br>{row["NOM"]}</div>',
             icon_size=(150, 50)
+        )
+    ).add_to(map_bnk)
+
+
+    # Graphique des effectifs
+    graph_data = create_graph(row)
+    folium.Marker(
+        location=[row['Y'], row['X']],
+        icon=folium.DivIcon(
+            html=f'<img src="{graph_data}" style="width: 100px; height: 100px;">'
         )
     ).add_to(map_bnk)
 
